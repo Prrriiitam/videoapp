48c90a26961aac1f66aa3de4416ede254d73fcd2
Pritam Kumar Sarangi <kumarsarangipritam@gmail.com>
2024-06-23 10:39:41 +0530

    updating server side index.js

--- server/index.js
+++ server/index.js
@@ -18,7 +18,13 @@ io.on("connection", (socket) => {
     io.to(room).emit("user:joined", {email, id: socket.id}); // Here when used wants to join to a room then we trigger this event and send the email and id to client
     socket.join(room); //Here when he wants to join then join
     io.to(socket.id).emit("room:join", data); // here first we storing the data comming from client to 2 maps and then on this line we again sending it to front end 
-    }
-)
+    });
+    socket.on("user:call", ({to, offer}) =>{
+        io.to(to).emit('incomming:call', {from: socket.id, offer}); // here we sending video to receipant
+    });
+
+    socket.on('call:accepted', ({to, ans}) => {
+        io.to(to).emit('call:accepted', {from: socket.id, ans});
+    });
 });
 
